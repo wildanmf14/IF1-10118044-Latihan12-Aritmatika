da0be7aa185671eea4c69d5ec5df46d008c046ad
Wildhevire <wildhevire404@gmail.com>
2019-09-26 18:06:23 +0000

    finished

---
+++ src/latihan/aritmatika/Aritmatika.java
@@ -0,0 +1,26 @@
+package latihan.aritmatika;
+
+public class Aritmatika {
+	/*
+    Nama        : Wildan Muhammad Fikri
+    Kelas       : IF1
+    NIM         : 10118044
+    Deskripsi   : Implementasi operasi aritmatika
+     */
+	 public static void main(String[] args) {
+	        int a = 10;
+	        int b = 20;
+	        int c = 25;
+	        int d = 25;
+	        System.out.println("a + b = " + (a + b));
+	        System.out.println("a - b = " + (a - b));
+	        System.out.println("a * b = " + (a * b));
+	        System.out.println("b / a = " + (b / a));
+	        System.out.println("b % a = " + (b % a));
+	        System.out.println("c % a = " + (c % a));
+	        System.out.println("a++ = " + (a++));
+	        System.out.println("a-- = " + (a--));
+	        System.out.println("d++ = " + (d++));
+	        System.out.println("++d = " + (++d));
+	    }
+}	
